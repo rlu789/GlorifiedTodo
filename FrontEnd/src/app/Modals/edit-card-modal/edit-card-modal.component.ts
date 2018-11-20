17b54adc599c87237601ccb63e0c06e4c4928979
DESKTOP-K1IMU4P\Richard <richard.lu@griffithuni.edu.au>
2018-11-20 12:57:52 +1000

    overflow bug

--- FrontEnd/src/app/Modals/edit-card-modal/edit-card-modal.component.ts
+++ FrontEnd/src/app/Modals/edit-card-modal/edit-card-modal.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Card, CardsService } from '../../Services/cards.service';
+import { HttpErrorResponse } from "@angular/common/http";
 
-import { MatDialogRef, MAT_DIALOG_DATA, } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-edit-card-modal',
@@ -13,19 +14,26 @@ export class EditCardModalComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<EditCardModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { card: Card }, public cardsService: CardsService) {
+    @Inject(MAT_DIALOG_DATA) public data: { card: Card }, public cardsService: CardsService,
+    public snackBar: MatSnackBar) {
     // console.log(data.card);
     this.tempCard = new Card(data.card.title, data.card.description, data.card.cardCollectionId);
     this.tempCard.id = data.card.id;
   }
 
-  updateCard(){
+  updateCard() {
     this.cardsService.update(this.tempCard).subscribe((data: Card) => {
       if (data) this.dialogRef.close(data);
       else alert("WTF HAPPENED");
+    }, (err: HttpErrorResponse) => {
+      console.log(err);
+      this.openSnackBar("To user: I.O.U one actual error message from dev");
     });
   }
 
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Close');
+  }
   onNoClick(): void {
     this.dialogRef.close();
   }
